d79cc24b0ee657c2a1ed40f34343a78a811c0d18
sarpisik <contact@sarpisik.com>
2019-05-27 21:37:48 +0200

    Add service worker & google analytics

--- src/index.js
+++ src/index.js
@@ -6,9 +6,29 @@ import './index.css';
 window.addEventListener('load', render);
 
 function render() {
+  // Registering Our Service Worker
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker
+      .register('/service-worker.js')
+      .then(registration => {
+        console.log('SW registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  } else {
+    window.alert(
+      'Hi! Welcome to Rock-Paper-Scissors game app. If you like to play the game offline, please visit back by using a different browser which supports service workers.'
+    );
+  }
+
   // Listen submit event
   document.querySelector('form').addEventListener('submit', handleSubmit);
 }
+
+navigator.onLine ||
+  window.alert('Looks like you are playing the game offline. Enjoy! :)');
+
 function handleSubmit(e) {
   e.preventDefault();
   let totalRoundCount = Number(document.querySelector('input').value);
